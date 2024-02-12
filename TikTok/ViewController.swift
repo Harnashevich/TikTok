4bb0765e617f92bd2888ce7bf69de4e7dd6e3040
Andrei <garnash1313@gmail.com>
2024-02-12 18:01:32 +0300

    init commit

--- TikTok/ViewController.swift
+++ TikTok/ViewController.swift
@@ -11,6 +11,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemRed
+        view.backgroundColor = .red
     }
 }
