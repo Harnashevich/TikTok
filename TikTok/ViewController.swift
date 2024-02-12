6853b0584dcad8f41dd6de213d6acf737f97ae61
Andrei <garnash1313@gmail.com>
2024-02-12 17:41:34 +0300

    Initial Commit

---
+++ TikTok/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TikTok
+//
+//  Created by Andrei Harnashevich on 12.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
